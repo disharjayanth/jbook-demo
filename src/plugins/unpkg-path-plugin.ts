63b857153399734a1213ee246b9d472252af6397
disharjayantha <dishuj15@gmail.com>
2021-03-21 16:55:35 +0530

    more esbuild

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,13 +10,15 @@ export const unpkgPathPlugin = () => {
                 if (args.path === "index.js") {
                     return { path: args.path, namespace: 'a' };
                 }
+
+                if (args.path.includes("./") || args.path.includes("../")) {
+                    return { path: new URL(args.path, "https://unpkg.com/" + args.resolveDir + "/").href, namespace: "a" };
+                }
+
                 return {
                     namespace: "a",
                     path: `https://unpkg.com/${args.path}`
                 };
-                // else if (args.path === "tiny-test-pkg") {
-                //     return { path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js", namespace: "a" };
-                // }
             });
 
             build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -26,17 +28,19 @@ export const unpkgPathPlugin = () => {
                     return {
                         loader: 'jsx',
                         contents: `
-              const message = require("tiny-test-pkg");
-              console.log(message);
+              import React from "react"
+              console.log(React, reactDOM);
             `,
                     };
                 }
 
-                const { data } = await axios.get(args.path);
+                const { data, request } = await axios.get(args.path);
+                console.log("Request:", request.responseURL);
                 console.log(data);
                 return {
                     loader: 'jsx',
-                    contents: data
+                    contents: data,
+                    resolveDir: new URL("./", request.responseURL).pathname
                 };
             });
         },
