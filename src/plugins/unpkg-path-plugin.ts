f91cd4d9a31bf0056429fc91e772ca816d8d1311
disharjayantha <dishuj15@gmail.com>
2021-03-22 19:48:07 +0530

    more esbuild

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,7 +1,12 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from "axios";
+import localforage from "localforage";
 
-export const unpkgPathPlugin = () => {
+const fileCache = localforage.createInstance({
+    name: "filecache"
+});
+
+export const unpkgPathPlugin = (inputCode: string) => {
     return {
         name: 'unpkg-path-plugin',
         setup(build: esbuild.PluginBuild) {
@@ -23,25 +28,32 @@ export const unpkgPathPlugin = () => {
 
             build.onLoad({ filter: /.*/ }, async (args: any) => {
                 console.log('onLoad', args);
-
                 if (args.path === 'index.js') {
                     return {
                         loader: 'jsx',
-                        contents: `
-              import React from "react"
-              console.log(React, reactDOM);
-            `,
+                        contents: inputCode,
                     };
                 }
 
+                // Check to see if we already fetched this file
+                // and if it is in cache 
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+                // if it is then return it immiditely
+                if (cachedResult) {
+                    return cachedResult;
+                }
+
                 const { data, request } = await axios.get(args.path);
-                console.log("Request:", request.responseURL);
-                console.log(data);
-                return {
+                const result: esbuild.OnLoadResult = {
                     loader: 'jsx',
                     contents: data,
                     resolveDir: new URL("./", request.responseURL).pathname
                 };
+                // otherwise store response in cache
+                await fileCache.setItem(args.path, result);
+
+                return result;
             });
         },
     };
