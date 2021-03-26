b3f8b7b8c524b0d20099ee0cbf4f1c6b47cb7129
disharjayantha <dishuj15@gmail.com>
2021-03-26 20:45:58 +0530

    iframe and more

--- src/index.tsx
+++ src/index.tsx
@@ -55,7 +55,7 @@ const App = () => {
                 <button onClick={onClick}>Submit</button>
             </div>
             <pre>{code}</pre>
-            <iframe src="/test.html" title="output"></iframe>
+            <iframe sandbox="allow-same-origin" src="/test.html" title="output"></iframe>
         </div>
     );
 };
